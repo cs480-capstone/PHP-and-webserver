cdb66a05937f6e0d83b4b6c1c52c07d11af0daf5
lukeclee <33207332+lukeclee@users.noreply.github.com>
2018-01-29 14:30:31 -0800

    Add files via upload

---
+++ treeinfo.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+This PHP script connects to the database and queries the Trees table to pull latitude, longitude, deocrators array, Special Events, and whether or not the tree is hidden and puts it into a json file.
+*/
+
+//server info
+$servername = "testdatabase.c2uw4uu5co9m.us-west-2.rds.amazonaws.com";
+$username = "lukewarm11";
+$password = "Leel1995!";
+$dbname = "testdb";
+
+//Make new sql connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+//check whether connection was succesful or not
+if(!$con){
+	die("connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM Trees"; //the actual query, pulls everything from Trees
+$result = mysqli_query($con, $sql); //holds whether the query was successful
+
+//if the result was successful then pull the information needed from database and put it into an array
+if(mysqli_num_rows($result) > 0) {
+	while($row = mysqli_fetch_assoc($result)){
+		$lat = $row['latitude'];
+		$long = $row['longitude'];
+		$decs = $row['Decorators'];
+		$special = $row['SpecialEvents'];
+		$hidden = $row['hidden'];
+
+		$trees[] = array('latitude' => $lat, 'longitude' => $long, 'Decorators' => $decs, 'SpecialEvents' => $special, 'hidden' => $hidden);
+	}
+}
+
+$response['trees'] = $trees; //creates array of the trees
+
+//encodes json data and creates a file called trees.json to store this data
+$json_data = json_encode($response, JSON_PRETTY_PRINT); 
+file_put_contents('trees.json', $json_data);
+
+mysqli_close($con); //close the connection
+?>
