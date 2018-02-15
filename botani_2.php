7060c8abb662bc4609753ded3f82854fd73fcb0a
Hermann Yepdjio <33042517+bonspi2000@users.noreply.github.com>
2018-02-15 11:35:42 -0800

    Add files via upload

---
+++ botani_2.php
@@ -0,0 +1,32 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+/*
+This PHP script connects to the database and queries the Trees table to pull latitude, longitude, deocrators array, Special Events, and whether or not the tree is hidden and puts it into a json file.
+*/
+//server info
+$servername = "testdatabase.c2uw4uu5co9m.us-west-2.rds.amazonaws.com";
+$username = "lukewarm11";
+$password = "Leel1995!";
+$dbname = "testdb";
+//Make new sql connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+//check whether connection was succesful or not
+if(!$con){
+	die("connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM Trees"; //the actual query, pulls everything from Trees
+$result = mysqli_query($con, $sql); //holds whether the query was successful
+//if the result was successful then pull the information needed from database and put it into an array
+if(mysqli_num_rows($result) > 0) {
+	while($row = mysqli_fetch_assoc($result))
+	{
+		
+		$trees[] = $row;
+	}
+}
+$response['trees'] = $trees; //creates array of the trees
+echo json_encode($trees);
+
+mysqli_close($con); //close the connection
+?>
