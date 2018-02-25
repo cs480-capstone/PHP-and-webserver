ffeff1a1533540649156edcd01ac235358c22fbe
Hermann Yepdjio <33042517+bonspi2000@users.noreply.github.com>
2018-02-25 08:26:50 -0800

    Add files via upload

---
+++ update_users.php
@@ -0,0 +1,32 @@
+<?php
+/*
+thiss PHP script retreives the posted data connects to the database and update the users table.
+*/
+//server info
+$servername = "testdatabase.c2uw4uu5co9m.us-west-2.rds.amazonaws.com";
+$username = "lukewarm11";
+$password = "Leel1995!";
+$dbname = "testdb";
+//Make new sql connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+//check whether connection was successful or not
+if(!$con)
+{
+	die("connection failed: " . mysqli_connect_error());
+}
+$json = file_get_contents('php://input'); //read the json data
+$json_data = json_decode($json); //pull the information from the json input
+//data that will be the same for each type of tree
+$id_user = (int) $json_data[0]['id_user']; //user information
+$trees = (int)$json_data[0]['trees']; //user's trees
+$username = $json_data[0]['username']; //username
+$points =(int) $json_data[0]['points']; //the number of points the user has
+$sqli = "UPDATE Users SET totalPoints = :points, trees = :trees WHERE idUsers = id_user ";
+$result = mysqli_query($con, $sqli); //result holds true or false
+//if there is an error it will print out what it is
+if($result===false)
+{
+	printf("error: %s\n", mysqli_error($con));
+}
+
+?>
---
+++ users.php
@@ -0,0 +1,38 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+/*
+This PHP script connects to the database and queries the Trees table to pull latitude, longitude, deocrators array, Special Events, and whether or not the tree is hidden and puts it into a json file.
+*/
+//server info
+$servername = "testdatabase.c2uw4uu5co9m.us-west-2.rds.amazonaws.com";
+$username = "lukewarm11";
+$password = "Leel1995!";
+$dbname = "testdb";
+//Make new sql connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+//check whether connection was succesful or not
+if(!$con)
+{
+	die("connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM Users ORDER BY totalpoints"; //the actual query, pulls everything from Users
+$result = mysqli_query($con, $sql); //holds whether the query was successful
+//if the result was successful then pull the information needed from database and put it into an array
+if(mysqli_num_rows($result) > 0) 
+{
+	while($row = mysqli_fetch_assoc($result))
+	{
+		$user_id = $row['idUsers'];
+		$username = $row['username'];
+		$totalPoints = (int)$row['totalPoints'];
+		$trees = (int)$row['trees'];
+		$users[] = array('user_id'=>$user_id, 'username' => $username, 'totalPoints' => $totalPoints, 'trees' => $trees);
+		
+	}
+}
+echo json_encode($users);
+
+mysqli_close($con); //close the connection
+?>
