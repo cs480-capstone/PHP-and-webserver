56db1da505a454ff8bb19aeb0c14759bf2fbae00
Hermann Yepdjio <33042517+Hermann-Yepdjio@users.noreply.github.com>
2018-03-06 22:52:37 -0800

    Add files via upload

---
+++ create_entry.php
@@ -0,0 +1,56 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+/*
+this PHP script connects to the database and inserts the collected data from the collectedData.json file.
+*/
+//server info
+$servername = "testdatabase.c2uw4uu5co9m.us-west-2.rds.amazonaws.com";
+$username = "lukewarm11";
+$password = "Leel1995!";
+$dbname = "testdb";
+//Make new sql connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+//check whether connection was successful or not
+if(!$con){
+	die("connection failed: " . mysqli_connect_error());
+}
+$json = file_get_contents("php://input"); //read json data
+$json_data = json_decode($json); //pull the information from the json file and put it into an array
+//data that will be the same for each type of tree
+$user = (int)$json_data->user; //user information
+$tree = (int)$json_data->tree; //tree id
+$time = date('y/m/d h:i:sa'); //the time that the collection occured
+$openPollenConesValue = (int)$json_data->openPollenConesValue; //breaking needle buds: integer
+$unfoldingLeavesValue = (int)$json_data->unfoldingLeavesValue; //Young needles: integer
+$fullSizedLeavesValue = (int)$json_data->fullSizedLeavesValue; //Pollen cones: integer
+$coloredLeavesValue = (int)$json_data->coloredLeavesValue; //Open pollen cones: percentage
+$openedFlowersValue = (int)$json_data->openedFlowersValue; //Pollen release: Little, some, lots
+$ripeFruitsValue = (int)$json_data->ripeFruitsValue; //Unripe seed cones: integer 
+$breakingNeedleBudsValue = (int)$json_data->breakingNeedleBudsValue; //Ripe seed cones: integer
+$youngNeedlesValue = (int)$json_data->youngNeedlesValue; //Recent cone or seed drop: integer
+$freshPollenConesValue = (int)$json_data->freshPollenConesValue; //Pollen release: Little, some, lots
+$unripeSeedConesValue = (int)$json_data->unripeSeedConesValue; //Unripe seed cones: integer 
+$ripeSeedConesValue = (int)$json_data->ripeSeedConesValue; //Ripe seed cones: integer
+$droppedSeedConesValue = (int)$json_data->droppedSeedConesValue; //Recent cone or seed drop: integer
+$breakingLeafBudsValue = (int)$json_data->breakingLeafBudsValue; //Pollen release: Little, some, lots
+$flowerBudsValue = (int)$json_data->flowerBudsValue; //Unripe seed cones: integer 
+$fruitsValue = (int)$json_data->fruitsValue; //Ripe seed cones: integer
+$droppedFruitsValue = (int)$json_data->droppedFruitsValue; //Recent cone or seed drop: integera
+$openPollenConesRadioValue = $json_data->openPollenConesRadioValue;//
+$coloredLeavesRadioValue = $json_data->coloredLeavesRadioValue;//
+
+
+
+
+
+	//the sql that will put the information into each column in the database 
+	$sqli = "INSERT INTO Entry (User, Tree, Timedate, openPollenConesValue, unfoldingLeavesValue, fullSizedLeavesValue, coloredLeavesValue, openedFlowersValue, ripeFruitsValue, breakingNeedleBudsValue, youngNeedlesValue, freshPollenConesValue, unripeSeedConesValue, ripeSeedConesValue, droppedSeedConesValue, breakingLeafBudsValue, flowerBudsValue, fruitsValue, droppedFruitsValue, openPollenConesRadioValue, coloredLeavesRadioValue  ) VALUES ($user, $tree, '$time', $openPollenConesValue, $unfoldingLeavesValue, $fullSizedLeavesValue, $coloredLeavesValue, $openedFlowersValue, $ripeFruitsValue, $breakingNeedleBudsValue, $youngNeedlesValue, $freshPollenConesValue, $unripeSeedConesValue, $ripeSeedConesValue, $droppedSeedConesValue, $breakingLeafBudsValue, $flowerBudsValue, $fruitsValue, $droppedFruitsValue, '$openPollenConesRadioValue', '$coloredLeavesRadioValue'  )";
+	$result = mysqli_query($con, $sqli);//result holds whether the query was succesful or not
+	//if it was not successful then it will print out the error
+	if($result===false){
+		printf("error: %s\n", mysqli_error($con));
+	}
+
+?>
--- update_users.php
+++ update_users.php
@@ -1,12 +1,15 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 /*
 thiss PHP script retreives the posted data connects to the database and update the users table.
 */
 //server info
-$servername = "";
-$username = "";
-$password = "";
-$dbname = "";
+$servername = "testdatabase.c2uw4uu5co9m.us-west-2.rds.amazonaws.com";
+$username = "lukewarm11";
+$password = "Leel1995!";
+$dbname = "testdb";
 //Make new sql connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
 //check whether connection was successful or not
@@ -16,12 +19,10 @@ if(!$con)
 }
 $json = file_get_contents('php://input'); //read the json data
 $json_data = json_decode($json); //pull the information from the json input
-//data that will be the same for each type of tree
-$id_user = (int) $json_data[0]['id_user']; //user information
-$trees = (int)$json_data[0]['trees']; //user's trees
-$username = $json_data[0]['username']; //username
-$points =(int) $json_data[0]['points']; //the number of points the user has
-$sqli = "UPDATE Users SET totalPoints = :points, trees = :trees WHERE idUsers = id_user ";
+//get uers' data
+$username =  $json_data->user_name; //user information
+$points = (int) $json_data->points; //the number of points the user has
+$sqli = "UPDATE Users SET totalPoints = totalPoints + $points WHERE username= '$username'";
 $result = mysqli_query($con, $sqli); //result holds true or false
 //if there is an error it will print out what it is
 if($result===false)
@@ -29,4 +30,4 @@ if($result===false)
 	printf("error: %s\n", mysqli_error($con));
 }
 
-?>
+
